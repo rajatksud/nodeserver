3ed6221bbfd3c9026fa985f59af63a36d5759637
rajatksud <rajatksud@yahoo.co.in>
2022-10-31 16:06:17 +0530

    Delete dev.js
    Should not be loaded

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-//dev.js dont commit this
-module.exports =
-{googleClientID: '681734354322-513nns09o1iqft4ohlh0bvstk7hgd8u4.apps.googleusercontent.com',
-googleClientSecret:'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',
-mongoURI: 'mongodb+srv://dbmanager:happy1234@cluster0.r6rcmov.mongodb.net/emaily?retryWrites=true&w=majority',
-cookieKey: 'weufhpeqw23823kjsdafhai32kjadssdafd'
-};
