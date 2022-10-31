1c3f5f9b09f509c213e01086f3f82fae02b79b2f
rajatksud <rajatksud@yahoo.co.in>
2022-10-31 15:29:59 +0530

    Error in config/keys modeule corrected

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //keys.js figure out what set of credentials to return
 if (process.env.node_env=='production'){
-    modeule.exports = require('./prod');
+    module.exports = require('./prod');
 } else {
     // we are in dev mode return dev keys
     module.exports = require('./dev');
